3918bbd3058e229ff33620e5faa2731f228c7992
CodeMeYulia <yupipper@gmail.com>
2023-04-05 17:27:04 +0300

    make flex proc and visual used button

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const prices = document.querySelectorAll('.basket__item_price');
 console.log(prices);
-let a = Number(prices[0].innerHTML);
-let b = Number(prices[1].innerHTML);
-let c = Number(prices[2].innerHTML);
-let d = Number(prices[3].innerHTML);
+const a = Number(prices[0].innerHTML);
+const b = Number(prices[1].innerHTML);
+const c = Number(prices[2].innerHTML);
+const d = Number(prices[3].innerHTML);
 
 console.log(a);
 console.log(b);
@@ -15,10 +15,12 @@ const totalsum = document.querySelector('.basket__total_sum');
 totalsum.innerHTML = a + b + c + d;
 
 let coupon = 20;
+document.querySelector('button').innerHTML = "Использовать купон на " + coupon + "%";
+
 const totaltotal = document.querySelector('.basket__total_total');
-let couponbutton = document.querySelector('button');
-let totaltotalsum = (totalsum.innerHTML * (100-20)) / 100;
-let saleSum = document.querySelector('.saleSum')
+const couponbutton = document.querySelector('button');
+const totaltotalsum = (totalsum.innerHTML * (100 - coupon)) / 100;
+const saleSum = document.querySelector('.saleSum')
 
 
 function makeClick(){
@@ -26,6 +28,7 @@ console.log('coupon used!');
 totaltotal.innerHTML  = totaltotalsum;
 saleSum.innerHTML = 'Итого со скидкой:';
 console.log(totaltotal.innerHTML);
+document.querySelector('button').innerHTML = "Купон применен!"
 }
 
 couponbutton.addEventListener('click', makeClick); 
